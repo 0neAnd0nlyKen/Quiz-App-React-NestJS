250d9923581d03548c918bf63091703be6dfe77e
0neAnd0nlyKen <kendrickraphael@gmail.com>
2026-02-04 21:00:05 +0800

    test railway env

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `Hello World! Hello database environment ${process.env.DATABASE_HOST}`;
   }
 }
